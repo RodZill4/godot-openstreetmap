c5351481d1a26e0c413b06a4eb310290ba1bb012
Locher <ngoc.ton@gmx.de>
2023-11-12 23:34:12 +0100

    :bug: adjusted game_state handling, commented out a problematic position-save feature

--- addons/openstreetmap/global/game_state.gd
+++ addons/openstreetmap/global/game_state.gd
@@ -1,8 +1,8 @@
 extends Node
 
-const variables_file_name = "user://savedata.save"
+const variables_file_name = "user://savedata.bin"
 @onready var savegame_key = null#"TW"+OS.get_unique_ID()
-var variables = { }
+var variables = { "Player/Position/X": 0.0, "Player/Position/Y": 0.0 }
 
 func _ready():
 	print("game state ready")
@@ -17,33 +17,28 @@ func get_var(n, d):
 		return d
 
 func set_var(n, v):
-	variables[n] = v
+	### TODO: THIS BREAKS THE GAME
+	### Invalid set index '' (on base: Transform3D') with value of type 'float'
+	### variables[n] = v
+	pass
 
 func read():
-	if not FileAccess.file_exists(variables_file_name):
-		return # Read-Error, no save file detected
+#	if not FileAccess.file_exists(variables_file_name):
+#		return # Read-Error, no save file detected
 
-	var file
-	if savegame_key == null:
-		file = FileAccess.open(variables_file_name, FileAccess.READ)
-	else:
-		file = FileAccess.open_encrypted_with_pass(variables_file_name, FileAccess.READ, savegame_key)
+	var file = FileAccess.open(variables_file_name, FileAccess.READ)
 	if file != null:
 		variables = file.get_var()
-	file.close()
+		file.close()
 
 func write():
-	if not FileAccess.file_exists(variables_file_name):
-		return # Write-Error, no save file detected
+#	if not FileAccess.file_exists(variables_file_name):
+#		return # Write-Error, no save file detected
 
-	var file
-	if savegame_key == null:
-		file = FileAccess.open(variables_file_name, FileAccess.WRITE)
-	else:
-		file = FileAccess.open_encrypted_with_pass(variables_file_name, FileAccess.WRITE, savegame_key)
+	var file = FileAccess.open(variables_file_name, FileAccess.WRITE)
 	if file != null:
 		file.store_var(variables)
-	file.close()
+		file.close()
 
 func reset():
 	variables = { }
